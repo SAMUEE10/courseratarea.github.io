3e6a0b571841a498e7e6d7bb8fe44f345effda74
SAMUEE10 <150634152+SAMUEE10@users.noreply.github.com>
2023-12-12 20:23:48 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// Wrap the entire contents of script.js inside of an IIFE
+(function () {
+  console.log("Script.js está cargado");  // Agrega esta línea
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+  // Loop over the names array and say either 'Hello' or "Good Bye"
+  for (var i = 0; i < names.length; i++) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
+
+    if (firstLetter === 'j') {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
+    }
+  }
+})();
